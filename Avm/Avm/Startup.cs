010ca392739a2a36aeb5251bceb01c56c1d8b123
burakkarabiyik <burakkarabiyik@mail.com.tr>
2016-07-29 16:26:10 +0300

    Kücük hatalar giderildi

--- Avm/Avm/Startup.cs
+++ Avm/Avm/Startup.cs
@@ -14,39 +14,43 @@ namespace Avm
             dbContext db;
             using ( db = new dbContext())
             {
-                db.Database.CreateIfNotExists();
-                
-            }
-             
-           
+                db.Database.CreateIfNotExists(); 
+            } 
             ApplicationDbContext database = new ApplicationDbContext();//Admin ekleme işlemini burada yapıyorum.
-            ApplicationUser user = new ApplicationUser();
-            user.Ad = "Admin";
-            user.Email = "admin@admin.com";
-            user.EmailConfirmed = true;
-            user.Id = "1";
-            user.Kullaniciadi = "Admin";
-            user.Soyad = "Admin";
-            user.Telno = "0000";
-            user.PasswordHash = "AL5c2IT+Tiem48wFc1fSmmg4zXIjE97NLB71EhbHYM7i9Ij54qsuL8hMci0ACvZFtQ==";
-            user.SecurityStamp = "5370dcc2-5b20-4ceb-b9ea-a1d2a7153fcc";//Şifre 123456 diye atandı .
-            user.Tutar = 0;
-            user.UserName = "admin@admin.com";
+          
+                ApplicationUser user = new ApplicationUser();
+                user.Ad = "Admin";
+                user.Email = "admin@admin.com";
+                user.EmailConfirmed = true;
+                user.Id = "1";
+                user.Kullaniciadi = "Admin";
+                user.Soyad = "Admin";
+                user.Telno = "0000";
+                user.PasswordHash = "AL5c2IT+Tiem48wFc1fSmmg4zXIjE97NLB71EhbHYM7i9Ij54qsuL8hMci0ACvZFtQ==";
+                user.SecurityStamp = "5370dcc2-5b20-4ceb-b9ea-a1d2a7153fcc";//Şifre 123456 diye atandı .
+                user.Tutar = 0;
+                user.UserName = "admin@admin.com";
 
+                if(database.Users.Find(user.Id)!=null)
+                {  }
+                else
+                {
+                    database.Users.Add(user);
 
-            database.Users.Add(user);
+                    database.SaveChanges();
+                    IdentityRole rol = new IdentityRole("Admin"); rol.Id = "1";
+                    database.Roles.Add(rol);//Admin Rolünü ekledik
+                    rol = new IdentityRole("Satıcı"); rol.Id = "2";
+                    database.Roles.Add(rol);//Satıcı Rolünü ekledik
+                    Microsoft.AspNet.Identity.EntityFramework.IdentityUserRole role = new Microsoft.AspNet.Identity.EntityFramework.IdentityUserRole();
+                    role.RoleId = "1";
+                    role.UserId = "1";
+                    user.Roles.Add(role); //Üyeliğimize Admin Rolümüzü ekledik.
+                    database.SaveChanges();
 
-            database.SaveChanges();
-            IdentityRole rol = new IdentityRole("Admin"); rol.Id = "1";
-            database.Roles.Add(rol);//Admin Rolünü ekledik
-             rol = new IdentityRole("Satıcı"); rol.Id = "2";
-            database.Roles.Add(rol);//Satıcı Rolünü ekledik
-            Microsoft.AspNet.Identity.EntityFramework.IdentityUserRole role = new Microsoft.AspNet.Identity.EntityFramework.IdentityUserRole();
-            role.RoleId = "1";
-            role.UserId = "1";
-            user.Roles.Add(role); //Üyeliğimize Admin Rolümüzü ekledik.
-            database.SaveChanges();
-
+                }
+                
+            
             ConfigureAuth(app);
         }
     }
